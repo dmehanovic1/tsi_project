e455420c43cdfac195db6e1a8d2a07328e999eb3
denin.mehanovic <denin.mehanovic@bstelecom.ba>
2024-11-18 18:27:57 +0100

    v1.0.1 Dodano pokretanje testova definiranih u pt_DFT.py

--- tests/common.py
+++ tests/common.py
@@ -1,8 +1,17 @@
 '''
-Testovi zajednički za sve module
+Pokretač svih testova
 '''
+import pytest
 
+def run_tests():
+    pytest.main(["pt_DFT.py"])
+
+run_tests()
 
 #   v1.0.0  @author Denin Mehanović
 #           @date   03.11.2024.
 #           @change Inicijalna verzija
+
+#   v1.0.1  @author Denin Mehanović
+#           @date   17.11.2024.
+#           @change Dodano pokretanje testova definiranih u pt_DFT.py
