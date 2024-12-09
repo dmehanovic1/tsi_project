fa80470a192fd65ce6b663011d108b27b7854240
acengic <75183723+acengic1@users.noreply.github.com>
2024-12-09 21:23:02 +0100

    v1.0.2
    Dodano pokretanje testova definiranih u pt_FFT.py i formatiran ispis izlaza pytest-a

--- tests/common.py
+++ tests/common.py
@@ -4,7 +4,8 @@ Pokretač svih testova
 import pytest
 
 def run_tests():
-    pytest.main(["pt_DFT.py"])
+    pytest.main(["pt_DFT.py", "-v", "--color=yes"])
+    pytest.main(["pt_FFT.py", "-v", "--color=yes"])
 
 run_tests()
 
@@ -15,3 +16,7 @@ run_tests()
 #   v1.0.1  @author Denin Mehanović
 #           @date   17.11.2024.
 #           @change Dodano pokretanje testova definiranih u pt_DFT.py
+
+#   v1.0.2  @author Amila Čengić
+#           @date   09.12.2024
+#           @change Dodano pokretanje testova definiranih u pt_FFT.py i formatiran ispis izlaza pytest-a
