c73ebfce9395a1a1505c71f6e29bae7cecb3cee5
aminaa181 <aomercevic1@etf.unsa.ba>
2024-12-09 20:54:19 +0100

    v1.0.1
    Uklonjen docstrings i dodani testovi za provjeru unosa

--- tests/pt_DFT.py
+++ tests/pt_DFT.py
@@ -1,15 +1,9 @@
-'''
-Testovi DFT modula
-'''
 import pytest
 import cmath
 import numpy as np
 from tsi_project.source.DFT import *
 
 def test_dft_t1():
-    '''
-    Jedinice
-    '''
     test_name = "dft_t1"
     x = [1,1,1,1]
     result = m_dft(x)
@@ -19,9 +13,6 @@ def test_dft_t1():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t2():
-    '''
-    Nule
-    '''
     test_name = "dft_t2"
     x = [0,0,0,0]
     result = m_dft(x)
@@ -31,21 +22,23 @@ def test_dft_t2():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t3():
-    '''
-    Prazan niz vrijednosti
-    '''
     test_name = "dft_t3"
     x = []
-    result = m_dft(x)
+    result = []
     expected = []
+    error = "test"
+    try:
+        result = m_dft(x)
+    except ValueError as e:
+        error = e
+
+    if error == ValueError("All elements of the list or np.ndarray must be numeric values (int or float or complex)."):
+        expected = error
 
     for r, e in zip(result, expected):
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t4():
-    '''
-    Realne vrijednosti
-    '''
     test_name = "dft_t4"
     x = [1,2,3,4]
     result = m_dft(x)
@@ -55,9 +48,6 @@ def test_dft_t4():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t5():
-    '''
-    Kompleksne vrijednosti
-    '''
     test_name = "dft_t5"
     x = [1 + 1j, 2 + 2j, 3 + 3j, 4 + 4j]
     result = m_dft(x)
@@ -67,9 +57,6 @@ def test_dft_t5():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t6():
-    '''
-    Neparan broj uzoraka
-    '''
     test_name = "dft_t6"
     x = [1,2,3]
     result = m_dft(x)
@@ -79,9 +66,6 @@ def test_dft_t6():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t7():
-    '''
-    Sinusni signal
-    '''
     test_name = "dft_t7"
     #0,duration,sample_rate*duration
     t = np.linspace(0,5,(int)(250*5))
@@ -94,9 +78,6 @@ def test_dft_t7():
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
 def test_dft_t8():
-    '''
-    Slučajni signal
-    '''
     test_name = "dft_t8"
     x = np.random.randn(100)
     result = m_dft(x)
@@ -105,6 +86,32 @@ def test_dft_t8():
     for r, e in zip(result, expected):
         assert abs(r-e) < 1e-6, f"{test_name}: Expected {e}, but got {r}"
 
+def test_dft_t9():
+    test_name = "dft_t9"
+    x = ["1,1,1,1","b"]
+    error = "ValueError"
+    result = []
+    try:
+        result = m_dft(x)
+    except ValueError as e:
+        error = e
+        if error == ValueError("Input must be a list or np.ndarray."):
+            assert 1, f"{test_name}: {error}"
+
+def test_dft_t10():
+    test_name = "dft_10"
+    x = 1
+    result = []
+    try:
+        result = m_dft(x)
+    except ValueError as e:
+        assert 1,  f"{test_name}: {e}"
+
+
 #   v1.0.0  @author Denin Mehanović
 #           @date   17.11.2024.
 #           @change Inicijalna verzija
+
+#   v1.0.1  @author Amina Omerčević
+#           @date   09.12.2024.
+#           @change Uklonjen docstrings i dodani testovi za provjeru unosa
